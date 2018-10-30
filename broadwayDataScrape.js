9b65bb8c47daf17dc627de7dfffc7f33815aa860
Bug Mika <bugtmika@gmail.com>
2018-10-30 13:10:39 -0400

    started working on data scrape scritpt

---
+++ broadwayDataScrape.js
@@ -0,0 +1,35 @@
+let name;
+let number;
+let optInDate;
+
+const Http = new XMLHttpRequest();
+const url='https://addmembers.com/rainmaker/contact.aspx?ID=6240797';
+Http.open("GET", url);
+Http.send();
+
+Http.onreadystatechange= function(){
+	if (this.readyState == 4 && this.status==200){
+		const siteData = Http.responseText;
+		const nameStringStart= siteData.indexOf("lblName");
+		const nameStringEnd= nameStringStart+100; 
+		const nameString= siteData.slice(nameStringStart, nameStringEnd);
+		const nameStart= nameString.indexOf(">") +1
+		const nameEnd= nameString.indexOf("<")
+		name=nameString.slice(nameStart,nameEnd)
+
+		const numberStringStart= siteData.indexOf("lblCellPhone");
+		const numberStringEnd= numberStringStart+100; 
+		const numberString= siteData.slice(numberStringStart, numberStringEnd);
+		const numberStart= numberString.indexOf(">") +1
+		const numberEnd= numberString.indexOf(" (")
+		number=numberString.slice(numberStart,numberEnd)
+
+		const optInStringStart= siteData.indexOf("lblDate");
+		const optInStringEnd= optInStringStart+100; 
+		const optInString= siteData.slice(optInStringStart, optInStringEnd);
+		const optInStart= optInString.indexOf(">") +1;
+		const optInEnd= optInString.indexOf("<");
+		optInDate=optInString.slice(optInStart,optInEnd);
+	
+	}
+}
