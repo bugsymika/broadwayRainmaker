dd1e8fc25c96370be835008e11cc22e3db70b996
Bug Mika <bugtmika@gmail.com>
2018-10-31 15:32:53 -0400

    fixed a bug where people with home phone numbers weren't getting their numbers added and added the process of merging the 3 arrays into 1

--- broadwayDataScrape.js
+++ broadwayDataScrape.js
@@ -1,7 +1,7 @@
 let name=[]
 let number=[]
 let optInDate=[]
-
+let prospectInfo= [] 
 // creates blank arrays to later be filled with prospect data 
 
 let webLeadsList= document.querySelectorAll('#webLeadsList a');
@@ -33,25 +33,48 @@ webLeads.forEach(function(element){
 			const numberString= siteData.slice(numberStringStart, numberStringEnd);
 			const numberStart= numberString.indexOf(">") +1;
 			const numberEnd= numberString.indexOf(" (");
-			number.push(numberString.slice(numberStart,numberEnd));
-
+			let cellNumber= numberString.slice(numberStart,numberEnd);
+			cellNumber = cellNumber.replace(/[- )(]/g,'');
+					if (cellNumber.length<10){
+							const numberStringStart= siteData.indexOf('"lblHomePhone"');
+							const numberStringEnd= numberStringStart+100; 
+							const numberString= siteData.slice(numberStringStart, numberStringEnd);
+							const numberStart= numberString.indexOf(">") +1;
+							const numberEnd= numberString.indexOf("(home)");
+							let cellNumber= numberString.slice(numberStart,numberEnd);
+							cellNumber = cellNumber.replace(/[- )(]/g,'');
+									if (cellNumber.length<10 || cellNumber.includes("span")===true) {
+									cellNumber="N/A";
+									number.push(cellNumber);	
+									}
+									else{
+							number.push(cellNumber);
+					}}
+		
+					else{
+							number.push(cellNumber);
+					}			
 			const optInStringStart= siteData.indexOf("lblDate");
 			const optInStringEnd= optInStringStart+100; 
 			const optInString= siteData.slice(optInStringStart, optInStringEnd);
 			const optInStart= optInString.indexOf(">") +1;
 			const optInEnd= optInString.indexOf("<");
 			optInDate.push(optInString.slice(optInStart,optInEnd));
+
+
+if (number.length===webLeads.length){
+	var prospects = name.map(function (x, i,) { 
+            prospect= [x, number[i],optInDate[i]]
+            prospectInfo.push(prospect); 
+            });
+		}
+
 	}
 }
 })
 
 // this script rips the names, numbers and opt in dates of all the prospects on the rainmaker home page and pushes them into 
-// the blank arrays previously created.
-
-
-
-
-
+// the blank arrays previously created. Once all the links have been parsed, the 3 arrays are merged.
 
 
 
