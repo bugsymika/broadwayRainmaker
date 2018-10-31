a3b825a54df78c60fd97820ec5e2401d77d1b908
Bug Mika <bugtmika@gmail.com>
2018-10-31 12:11:02 -0400

    changed the data scrape to push to an array, working on creating a more optimized method to comb the HMTL

--- broadwayDataScrape.js
+++ broadwayDataScrape.js
@@ -1,9 +1,9 @@
-let name;
-let number;
-let optInDate;
+let name=[]
+let number=[]
+let optInDate=[]
 
 const Http = new XMLHttpRequest();
-const url='https://addmembers.com/rainmaker/contact.aspx?ID=6240797';
+const url='https://addmembers.com/rainmaker/contact.aspx?ID=6232914';
 Http.open("GET", url);
 Http.send();
 
@@ -13,23 +13,29 @@ Http.onreadystatechange= function(){
 		const nameStringStart= siteData.indexOf("lblName");
 		const nameStringEnd= nameStringStart+100; 
 		const nameString= siteData.slice(nameStringStart, nameStringEnd);
-		const nameStart= nameString.indexOf(">") +1
-		const nameEnd= nameString.indexOf("<")
-		name=nameString.slice(nameStart,nameEnd)
+		const nameStart= nameString.indexOf(">") +1;
+		const nameEnd= nameString.indexOf("<");
+		name.push(nameString.slice(nameStart,nameEnd));
 
 		const numberStringStart= siteData.indexOf("lblCellPhone");
 		const numberStringEnd= numberStringStart+100; 
 		const numberString= siteData.slice(numberStringStart, numberStringEnd);
-		const numberStart= numberString.indexOf(">") +1
-		const numberEnd= numberString.indexOf(" (")
-		number=numberString.slice(numberStart,numberEnd)
+		const numberStart= numberString.indexOf(">") +1;
+		const numberEnd= numberString.indexOf(" (");
+		number.push(numberString.slice(numberStart,numberEnd));
 
 		const optInStringStart= siteData.indexOf("lblDate");
 		const optInStringEnd= optInStringStart+100; 
 		const optInString= siteData.slice(optInStringStart, optInStringEnd);
 		const optInStart= optInString.indexOf(">") +1;
 		const optInEnd= optInString.indexOf("<");
-		optInDate=optInString.slice(optInStart,optInEnd);
-	
+		optInDate.push(optInString.slice(optInStart,optInEnd));
+
 	}
 }
+
+
+
+
+
+
